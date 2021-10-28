6507178518eeac2f7b7a2845ae9b67732f9c9981
HaiAnh <haianhnguyen12082001>
2021-10-28 16:54:13 +0700

    update

---
+++ .history/assets/js/main_20211028153602.js
@@ -0,0 +1,366 @@
+$(document).ready(function(){
+    $(".banner").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 3000,
+    });
+    $(".feedback__container").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 3000,
+    });
+    $(".slide__list").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 3000,
+        responsive:[
+            {
+                breakpoint: 1024,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    speed: 2000,
+                    autoplaySpeed: 3000,
+                }
+              },
+            {
+                breakpoint: 768,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    speed: 2000,
+                    autoplaySpeed: 3000,
+                }
+              },
+              {
+                breakpoint: 576,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    speed: 2000,
+                    autoplaySpeed: 3000,
+                }
+              },
+        ]
+    });
+    $(".discout__conatiner").slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 3000,
+        responsive:[
+            {
+                breakpoint: 1024,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    speed: 2000,
+                    autoplaySpeed: 3000,
+                }
+              },
+            {
+                breakpoint: 768,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    speed: 2000,
+                    autoplaySpeed: 3000,
+                }
+              },
+              {
+                breakpoint: 576,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    speed: 2000,
+                    autoplaySpeed: 3000,
+                }
+              },
+        ]
+    });
+    $(".sponsor__content").slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 3000,
+        responsive:[
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 2,
+                    arrows: false,
+                    autoplay: true,
+                    speed: 2000,
+                    autoplaySpeed: 3000,
+                }
+              },
+              {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    speed: 2000,
+                    autoplaySpeed: 3000,
+                }
+              },
+              {
+                breakpoint: 576,
+                settings: {
+                  
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    speed: 2000,
+                    autoplaySpeed: 3000,
+                }
+              },
+        ]
+    }); 
+ 
+    $(".sponsor__content").slick("refresh");
+    
+
+    $(".news_content").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 3000,
+        responsive:[
+            {
+                breakpoint: 768,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    speed: 2000,
+                    autoplaySpeed: 3000,
+                }
+              },
+        ]
+    });
+    $('.header__bar').click(function(){
+        $('.header__list').toggleClass('active');
+        $('.opacity__menu').toggleClass('open__opacity');
+        $(this).find(".hambergerIcon").toggleClass("open");
+        // console.log($('.open'))
+        
+    })
+    $('.opacity__menu').click(function(){
+        $('.header__list').removeClass('active');
+        $('.opacity__menu').removeClass('open__opacity');
+        $('.hambergerIcon').removeClass('open')
+    })
+    
+    ////TabProduct
+    $('.tabProduct__list').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: false,
+        asNavFor: '.tabProduct__nav', 
+      });
+      $('.tabProduct__nav').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.tabProduct__list',
+        // centerMode: true,
+        focusOnSelect: true,
+        prevArrow: ('<div class="slick-prev"><i class="fas fa-chevron-left"></i></div>') ,
+        nextArrow: ('<div class="slick-next"><i class="fas fa-chevron-right"></i></div>'),
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                },
+            },
+              {
+                breakpoint: 500,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                }
+                },
+        ],
+      });
+
+    /////WORKSHOP
+    $(".workshop__content").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 3000,
+        dots:true,
+        prevArrow: ('<div class="slick-prev"><i class="fas fa-chevron-left"></i></div>') ,
+        nextArrow: ('<div class="slick-next"><i class="fas fa-chevron-right"></i></div>'),
+    })
+  
+})
+
+
+var tabLinks = document.querySelectorAll(".category__link");
+var tabContent = document.querySelectorAll(".category__tab");
+
+tabLinks.forEach(function (el) {
+    el.addEventListener("click", openTabs);
+});
+
+function openTabs(el) {
+    var btn = el.currentTarget; // lắng nghe sự kiện và hiển thị các element
+    var electronic = btn.dataset.electronic; // lấy giá trị trong data-electronic
+
+    tabContent.forEach(function (el) {
+        el.classList.remove("active");
+    });
+
+    tabLinks.forEach(function (el) {
+        el.classList.remove("active");
+    });
+
+    document.querySelector("#" + electronic).classList.add("active");
+
+    btn.classList.add("active");
+    console.log(electronic);
+}
+
+/// Back To Top
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+/// SEARCH
+$('.fas.fa-search').click(function(){
+    $('.header__form').toggleClass('active');
+   
+})
+
+// Số lượng sẩn phẩm
+// let add = document.getElementById('add');
+// let remove = document.getElementById('minus')
+
+// let int = document.getElementById('number')
+// let integer = 0;
+// int.value = integer;
+
+// console.log(int.value);
+
+// add.addEventListener('click',function(){
+//     integer += 1;
+//     int.value = integer;
+// })
+
+// remove.addEventListener('click',function(){
+//     integer -= 1;
+//     int.value = integer;
+
+//     if(integer <1){
+//         integer = 1;
+        
+//     }
+// })
+// document.querySelector('.add').setAttribute("disabled","disabled");
+var valueCount
+let add = document.querySelector(".add");
+let remove = document.querySelector(".minus");
+valueCount = document.getElementById("number").value;
+// const number = document.querySelector('#number')
+// number.addEventListener('blur', function() {
+//    valueCount = document.getElementById("number").value;
+   
+// })
+// console.log(valueCount);
+add.addEventListener('click', function(){
+    valueCount = document.getElementById("number").value;
+
+    valueCount++;
+
+    document.getElementById("number").value = valueCount
+    if(valueCount > 1){
+        document.querySelector('.minus').removeAttribute("disabled")
+        document.querySelector('.minus').classList.remove("disabled")
+    }
+
+})
+// document.querySelector('.minus').setAttribute("disabled","disabled");
+// var valueCount
+// let add = document.querySelector(".add");
+
+remove.addEventListener('click', function(){
+    valueCount = document.getElementById("number").value;
+    
+    valueCount--;
+    if(valueCount<1){
+        valueCount=1;
+    }
+    document.getElementById("number").value = valueCount
+    if(valueCount == 1){
+        document.querySelector('.minus').setAttribute("disabled","disabled")
+    }
+})
